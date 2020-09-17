6392ab56a6bd575a16798f55bd8ed162c307b4f1
Avinash Kumar <avinashbest3@gmail.com>
2020-09-17 23:56:59 +0530

    ninth commit

--- Pointers_In_CPP/Program9.cpp
+++ Pointers_In_CPP/Program9.cpp
@@ -2,16 +2,19 @@
 Application of Pointer:
 7. Used for accessing array elements
 */
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 //int* ptr = arr; or int ptr[] = arr; ==> both are same in  taking array as an argument
-void fun(int* ptr, int n){
-  for (int i = 0; i < n; i++) {
-    cout << ptr[i] << " ";//ptr[i] read by compiler by *(ptr+i)
+void fun(int *ptr, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << ptr[i] << " "; //ptr[i] read by compiler by *(ptr+i)
   }
 }
 
-int main() {
+int main()
+{
   int arr[] = {10, 20, 30, 40, 50};
   fun(arr, 5);
   return 0;
